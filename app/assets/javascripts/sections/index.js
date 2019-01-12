3df0e99dc30dbc710d841ffcd1e678b5b1e240aa
manuchap <manuchap@gmail.com>
2019-01-12 22:46:07 +0100

    footer that crashes the app

--- app/assets/javascripts/sections/index.js
+++ app/assets/javascripts/sections/index.js
@@ -8,3 +8,4 @@ export { default as SectionOne } from './section_one';
 export { default as SectionTwo } from './section_two';
 export { default as SectionThree } from './section_three';
 export { default as SectionFour } from './section_four';
+export { default as Footer } from './footer';
