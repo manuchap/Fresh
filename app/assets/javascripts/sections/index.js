5c72fd855aea37208042a2c12eba08053ed4d384
manuchap <manuchap@gmail.com>
2019-01-13 13:16:17 +0100

    import hero

--- app/assets/javascripts/sections/index.js
+++ app/assets/javascripts/sections/index.js
@@ -1,8 +1,5 @@
 // Import all the sections here
-//
-// Example:
-// export { default as Hero } from './hero';
-//
+export { default as Hero } from './hero';
 export { default as Header } from './header';
 export { default as SectionOne } from './section_one';
 export { default as SectionTwo } from './section_two';
