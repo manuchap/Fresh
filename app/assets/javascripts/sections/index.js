5947b1e94cb32481d36f29e8f0cfe5be830d1df6
manuchap <manuchap@gmail.com>
2019-01-13 13:23:47 +0100

    Update index.js

--- app/assets/javascripts/sections/index.js
+++ app/assets/javascripts/sections/index.js
@@ -1,6 +1,4 @@
 // Import all the sections here
-//
-// Example:
 export { default as Hero } from './hero';
 export { default as Header } from './header';
 export { default as SectionOne } from './section_one';
