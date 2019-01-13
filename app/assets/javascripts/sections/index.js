123b07fe545a055ab75daa1314e91256724293b9
manuchap <manuchap@gmail.com>
2019-01-13 12:03:56 +0100

    hero bug

--- app/assets/javascripts/sections/index.js
+++ app/assets/javascripts/sections/index.js
@@ -1,8 +1,7 @@
 // Import all the sections here
 //
 // Example:
-// export { default as Hero } from './hero';
-//
+export { default as Hero } from './hero';
 export { default as Header } from './header';
 export { default as SectionOne } from './section_one';
 export { default as SectionTwo } from './section_two';
